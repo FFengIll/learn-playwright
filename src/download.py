db0c9c453e42cbcac54c3ce1ee6a2adbd5912a1c
FFengIll <fengyouzheng@gmail.com>
2023-03-21 10:34:33 +0800

    refactor: extract playwright download action.

--- src/download.py
+++ src/download.py
@@ -1,8 +1,29 @@
 from path import Path
 
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
 from util import holdon
+
+
+def download_in_chromium(page: "Page", action, output=None):
+    """
+    action is a callback to process the page
+
+    """
+    with page.expect_download() as download_info:
+        action(page)
+
+    download = download_info.value
+    # wait for download to complete
+    path = download.path()
+    print(download)
+    # it is a temp path for browser, call `save_as` for yours
+    print(path)
+
+    if output:
+        download.save_as(output)
+
 
 with sync_playwright() as p:
     browser_type = p.chromium
@@ -13,33 +34,22 @@ with sync_playwright() as p:
     # here is a demo website provide a download button (to a pdf)
     page.goto("https://www.mjrui.com/mjrui_FileInfo_364333.html")
 
-    with page.expect_download() as download_info:
+    def action(page: "Page"):
         button = page.query_selector("#HyperLink_Down")
         print(button)
 
         # add modifiers according to https://github.com/microsoft/playwright-python/issues/675
         # since `pdf` will lead to a view but download
         # and this works for chrome since `alt+left click` = `save as`
-        
+
         button.click(
             modifiers=[
                 "Alt",
             ]
         )
 
-    download = download_info.value
-    # wait for download to complete
-    path = download.path()
-    print(download)
-    # it is a temp path for browser, call `save_as` for yours
-    print(path)
-
     output = Path("output")
     if not output.exists():
         output.makedirs_p()
-    download.save_as(output / "test.pdf")
 
-    holdon()
-
-    
-    
+    download_in_chromium(page, action, output / "test.pdf")
